989d7b809ce94c131e69c5d5c8a19c2229bcbc3f
Цырков Георгий Александрович <g@tias.pro>
2022-05-16 20:57:32 +0300

    some control added

--- Wnd.cs
+++ Wnd.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 using OpenTK;
 using OpenTK.Graphics;
@@ -5,16 +6,34 @@ using OpenTK.Graphics.OpenGL;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
 
 namespace CadDev
 {
     public class CadWindow : OpenTK.Windowing.Desktop.GameWindow
     {
+        Matrix4 model;
+        Matrix4 view;
+        Matrix4 projection;
+
+        float speed = 1.5f;
+
+        Vector3 position = new Vector3(0.0f, 0.0f,  3.0f);
+        Vector3 front = new Vector3(0.0f, 0.0f, -1.0f);
+        Vector3 up = new Vector3(0.0f, 1.0f,  0.0f);
+
+        Vector2 lastPos;
+
         public CadWindow(GameWindowSettings gameWindowSettings, 
                          NativeWindowSettings nativeWindowSettings) : 
                          base(gameWindowSettings, nativeWindowSettings)
         {
+                CursorVisible = false;
+                CursorGrabbed = true;
 
+            model = Matrix4.CreateRotationX(MathHelper.DegreesToRadians(-55.0f));
+            view = Matrix4.CreateTranslation(0.0f, 0.0f, -3.0f);
         }
 
         // Points of a triangle in normalized device coordinates.
@@ -28,11 +47,7 @@ namespace CadDev
         int VertexBufferObject;
         int VertexArrayObject;
 
-        Shader shader;
-
-        Matrix4 model;
-        Matrix4 view;
-        Matrix4 projection;
+        Shader shader;        
 
         protected override void OnLoad()
         {   
@@ -49,16 +64,6 @@ namespace CadDev
 
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3*sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
-
-
-            Vector3 cameraPos = new Vector3(0.0f, 0.0f, 3.0f);
-            Vector3 cameraTarget = Vector3.Zero;
-            Vector3 cameraDirection = Vector3.Normalize(cameraPos - cameraTarget);
-
-            Vector3 up = Vector3.UnitY;
-            Vector3 cameraRight = Vector3.Normalize(Vector3.Cross(up, cameraDirection));
-            Vector3 cameraUp = Vector3.Cross(cameraDirection, cameraRight);
-
 
             base.OnLoad();
         }
@@ -84,8 +89,7 @@ namespace CadDev
 
             GL.Viewport(0, 0, Width, Height);
 
-            model = Matrix4.CreateRotationX(MathHelper.DegreesToRadians(-55.0f));
-            view = Matrix4.CreateTranslation(0.0f, 0.0f, -3.0f);
+
             projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), (float)((float)Width / (float)Height), 0.1f, 100.0f);
 
             base.OnResize(e);
@@ -97,6 +101,8 @@ namespace CadDev
 
             shader.Use();
 
+            view = Matrix4.LookAt(position, position + front, up);
+
             shader.SetMatrix4("model", model);
             shader.SetMatrix4("view", view);
             shader.SetMatrix4("projection", projection);
@@ -107,11 +113,115 @@ namespace CadDev
             Context.SwapBuffers();
             base.OnRenderFrame(e);
 
-            var errorCode = GL.GetError();
-            if (errorCode != ErrorCode.NoError)
+            OpenTK.Graphics.OpenGL.ErrorCode errorCode = GL.GetError();
+            if (errorCode != OpenTK.Graphics.OpenGL.ErrorCode.NoError)
             {
                 System.Console.WriteLine($"errorCode: {errorCode}");
             }
+        }
+
+
+        bool firstMove = true;
+        float sensitivity = 0.05f;
+        float yaw = -90;
+        float pitch = 0;
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            if (!IsFocused) // check to see if the window is focused
+            {
+                return;
+            }
+
+            if (KeyboardState.IsKeyDown(Keys.W))
+            {
+                position += front * speed * (float)e.Time; //Forward 
+            }
+
+            if (KeyboardState.IsKeyDown(Keys.S))
+            {
+                position -= front * speed * (float)e.Time; //Backwards
+            }
+
+            if (KeyboardState.IsKeyDown(Keys.A))
+            {
+                position -= Vector3.Normalize(Vector3.Cross(front, up)) * speed * (float)e.Time; //Left
+            }
+
+            if (KeyboardState.IsKeyDown(Keys.D))
+            {
+                position += Vector3.Normalize(Vector3.Cross(front, up)) * speed * (float)e.Time; //Right
+            }
+
+            if (KeyboardState.IsKeyDown(Keys.Space))
+            {
+                position += up * speed * (float)e.Time; //Up 
+            }
+
+            if (KeyboardState.IsKeyDown(Keys.LeftShift))
+            {
+                position -= up * speed * (float)e.Time; //Down
+            }
+
+            //if (MouseState.WasButtonDown(MouseButton.Left))
+            //{
+            //    firstMove = true;
+            //}
+
+            if (KeyboardState.IsKeyDown(Keys.C))
+            {
+                firstMove = true;
+                yaw = -90;
+                pitch = 0;
+
+                view = Matrix4.LookAt(new Vector3(0,0,-3), new Vector3(0,0,0), up);
+            }
+
+
+            if (MouseState.IsButtonDown(MouseButton.Left))// && KeyboardState.IsKeyDown(Keys.E))
+            {
+                if (firstMove)
+                {
+                    lastPos = new Vector2(MousePosition.X, MousePosition.Y);
+                    firstMove = false;
+                }
+                else
+                {
+
+                    float deltaX = MousePosition.X - lastPos.X;
+                    float deltaY = MousePosition.Y - lastPos.Y;
+
+                    lastPos = new Vector2(MousePosition.X, MousePosition.Y);
+
+                    yaw += deltaX * sensitivity;
+                    if (pitch > 89.0f)
+                    {
+                        pitch = 89.0f;
+                    }
+                    else if (pitch < -89.0f)
+                    {
+                        pitch = -89.0f;
+                    }
+                    else
+                    {
+                        pitch -= deltaY * sensitivity;
+                    }
+                }
+            }
+
+            if (KeyboardState.IsKeyReleased(Keys.E))
+            {
+                //CursorVisible = true;
+                //CursorGrabbed = false;
+                //firstMove = true;
+            }
+
+            front.X = (float)Math.Cos(MathHelper.DegreesToRadians(pitch)) * (float)Math.Cos(MathHelper.DegreesToRadians(yaw));
+            front.Y = (float)Math.Sin(MathHelper.DegreesToRadians(pitch));
+            front.Z = (float)Math.Cos(MathHelper.DegreesToRadians(pitch)) * (float)Math.Sin(MathHelper.DegreesToRadians(yaw));
+            front = Vector3.Normalize(front);
+
+
         }
     }
 }
